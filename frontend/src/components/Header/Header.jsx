79220c219330c17ce8556ddf983d325171bcec95
john carl <johnmurillo758@gmail.com>
2025-01-26 17:37:42 +0800

    ecommerce frontend reactjs

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Oder your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat necessitatibus aspernatur iusto recusandae error itaque at minima dignissimos omnis eos iste id ullam maiores fugiat, nostrum aliquam delectus consequatur neque.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
